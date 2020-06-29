c40cc30627a849930251f31dc67bc63c889bd10c
Luis Cova <luis.aca1997@gmail.com>
2020-06-29 17:42:57 -0300

    Se completo el reto numero 2 se deja Comentario de las instrucciones

---
+++ Reto 1/2_Hola_Nombre.py
@@ -0,0 +1,9 @@
+#Reto #2 “Hola… nombre y apellido:”
+#Instrucciones: Ahora que sabemos incluir nombres,
+#podemos agregar más datos.
+#Intentemos con un apellido para tener algo así:
+#``Hola, [nombre] [apellido]``
+
+name = input("Hola! ingresa un nombre: ")
+lastname = input("Bien hecho!, ahora ingresa un apellido: ")
+print("Hola, " + name + " " + lastname )
